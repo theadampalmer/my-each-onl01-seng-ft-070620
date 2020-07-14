1b81eeb671ad8e41f87124e8d5065407f8d23507
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 14:34:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each
+end  
+
+def my_each(words)
+  i = 0
+	while i < words.length
+# 	binding.pry
+		yield(words[i])
+    i += 1
+	end
+	words
 end
