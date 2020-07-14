42cfdc66011c9d8671be0e8b00e827b242ec4d84
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 14:33:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,11 @@
-def my_each # put argument(s) here
-  # code here
-end 
+def my_each
+end  
+
 def my_each(words)
   i = 0
-
 	while i < words.length
 		yield(words[i])
-   i += 1
+    i += 1
 	end
 	words
 end
